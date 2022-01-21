96a2c050a557fb54f50fe8f0585be7309cf002d8
Jean Michel Battirola <jeanbattirola@gmail.com>
2022-01-21 12:16:19 +0100

    ex 1.2 done

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,43 +1,31 @@
 import '@reach/dialog/styles.css'
-import Dialog from '@reach/dialog'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
 
 function App() {
   const [openModal, setOpenModal] = React.useState('none')
-  const close = () => setOpenModal('none')
 
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-
       <div>
         <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
-
       <div>
         <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
-
-      <Dialog
-        aria-label="Login form"
-        isOpen={openModal === 'login'}
-        onDismiss={close}
-      >
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
         <div>
-          <button onClick={close}>Close</button>
+          <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Login</h3>
       </Dialog>
-      <Dialog
-        aria-label="Registration form"
-        isOpen={openModal === 'register'}
-        onDismiss={close}
-      >
+      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
         <div>
-          <button onClick={close}>Close</button>
+          <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Register</h3>
       </Dialog>
